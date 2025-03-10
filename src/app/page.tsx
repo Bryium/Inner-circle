e7ea201a82eef165bbc9f0f638bdfebb122779e5
Bryium <bryiumonyancha@gmail.com>
2025-03-10 15:08:46 +0300

    add login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
 import { Button } from "@heroui/button";
+import Link from "next/link";
+import { FaRegSmile } from "react-icons/fa";
 
 export default function Home() {
   return (
     <div>
-      <h1 className="text-xl">Hello</h1>
-      <Button color="danger" variant="bordered">
+      <h1 className="text-3xl">Hello app!</h1>
+      <Button
+        as={Link}
+        href="/members"
+        color="primary"
+        variant="bordered"
+        startContent={<FaRegSmile size={20} />}
+      >
         Click me
       </Button>
     </div>
