808acfc04bc5302ba7aeb30c8a3905991d9a236e
Bryium <bryiumonyancha@gmail.com>
2025-05-29 15:46:57 +0300

    add styling to members list and improve ui

--- src/app/members/page.tsx
+++ src/app/members/page.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import { getMembers } from "../actions/MemberActions";
+import MemberCard from "./MembersCard";
 
 export default async function MembersPage() {
   const members = await getMembers();
   return (
-    <div>{members && members.map(({ name }) => <p key={name}>{name}</p>)}</div>
+    <div className="mt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-6 gap-8">
+      {members &&
+        members.map((member) => <MemberCard key={member.id} member={member} />)}
+    </div>
   );
 }
