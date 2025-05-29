baec809a8e4a1e666b115910d9e2faf60fab5813
Bryium <bryiumonyancha@gmail.com>
2025-05-29 15:29:16 +0300

    add fuction to display and render member list

--- src/app/members/page.tsx
+++ src/app/members/page.tsx
@@ -1,11 +1,9 @@
-import Link from "next/link";
 import React from "react";
+import { getMembers } from "../actions/MemberActions";
 
-export default function MembersPage() {
+export default async function MembersPage() {
+  const members = await getMembers();
   return (
-    <div>
-      <h3 className="text-3xl">This will be the members page</h3>
-      <Link href="/">Go back home</Link>
-    </div>
+    <div>{members && members.map(({ name }) => <p key={name}>{name}</p>)}</div>
   );
 }
